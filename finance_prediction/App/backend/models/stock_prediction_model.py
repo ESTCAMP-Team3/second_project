be7ff247f6033bfa2b12fcbbc00ba3c3f60bb669
doyoungEST <theopenhand@naver.com>
2025-06-25 13:45:19 +0900

    feat: 예측 메소드 추가

---
+++ finance_prediction/App/backend/models/stock_prediction_model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import os
+from datetime import datetime, timedelta
+import logging
+# 모델 프레임워크 추가
+
+class StockPredictionModel:
+
+    # 특성 데이터 준비
+    # 모델 학습
+    # 저장된 모델 로드
+
+    # 예측 실행
+    def predict(self, symbol, data, periods=["short", "medium", "long"]):
+        predictions = {}
+
+        return  predictions
+
