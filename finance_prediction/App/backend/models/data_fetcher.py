53f23145703318aeddd023e9eba31bf3854a3a1b
doyoungEST <theopenhand@naver.com>
2025-06-25 12:02:00 +0900

    feat: 주식 데이터 요청 추가

---
+++ finance_prediction/App/backend/models/data_fetcher.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+import requests
+import logging
+
+class DataFetcher:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    # 주식 데이터 요청
+    def get_stock_data(self, symbol, period = '1y'):
+        try:
+            ticker = yf.Ticker(symbol)
+            data = ticker.history(period=period)
+
+            if data.empty:
+                raise ValueError(f"{symbol} 해당 심볼에 해당하는 데이터를 찾을 수 없음.")
+
+            return data
+        except Exception as e:
+            self.logger.error(f"ERROR: {e}")
+            raise
